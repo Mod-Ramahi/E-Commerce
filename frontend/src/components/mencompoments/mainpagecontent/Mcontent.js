133f82f864833a9b5e1c889cf7c6ee5669ac72db
Mod-Ramah <m.ramahi989@gmail.com>
2023-05-19 04:00:55 +0300

    first content

--- frontend/src/components/mencompoments/mainpagecontent/Mcontent.js
+++ frontend/src/components/mencompoments/mainpagecontent/Mcontent.js
@@ -2,7 +2,32 @@ import './Mcontent.scss'
 
 const Mcontent = () => {
     return(
-        <p>Main page content</p>
+        <div className='content'>
+            <div className='left'>
+                <div className='description'>
+                    <div className='frst'>
+                        <div className='blogimg'>
+                            <img alt='blogimage' src='/images/b1.jpg'/>
+                        </div>
+                        <div className='blogbtn'>
+                            <button>More Details</button>
+                        </div>
+                    </div>
+                    <div className='scnd'>
+                        <h3>3 Incredible Tongkat Ali (Longjack) Benefits: Testosterone, Stamina, and More</h3>
+                        <span> Tongkat ali has been traditionally used for centuries to support the health of many individuals throughout Southeast Asia. Now this herb is enjoying worldwide exposure to the health benefit of millions. What is tongkat ali?
+
+What Is Tongkat Ali?
+Tongkat ali, also known by its scientific name Eurycoma longifolia, is a tall, thin, shrub-like plant native to countries in Southeast Asia, including Malaysia, Thailand, and Indonesia. Used as a traditional medicinal herb for centuries, tongkat ali is often known for its potential benefits for sexual health. </span>
+                    </div>
+                </div>
+            </div>
+                <div className='right'>
+
+                </div>
+
+        </div>
+        
     )
 }
 
